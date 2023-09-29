ab6593079fd76d924353300921e43bea8975a115
kthss01 <kthss01@naver.com>
2023-09-29 13:22:25 +0900

    init ArcadeEntity

---
+++ js/entities/ArcadeEntity.js
@@ -0,0 +1,23 @@
+export default class ArcadeEntity extends Phaser.Physics.Arcade.Sprite {
+	constructor(data) {
+		let { name, scene, x, y, texture, frame } = data;
+
+		super(scene.arcade.word, x, y, texture, frame);
+
+		this.x += this.width / 2;
+		this.y += this.height / 2;
+
+		this.name = name;
+		this._position = new Phaser.Math.Vector2(this.x, this.y);
+		this.scene.add.existing(this);
+	}
+
+	get position() {
+		this._position.set(this.x, this.y);
+		return this._position;
+	}
+
+	get velocity() {
+		return this.body.velocity;
+	}
+}
